f0d5b88da1947fd2c3d3a9a9f6379f3808e3c3d0
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-05 09:57:03 +0100

    Completamos el bucle while Creamos todas las condiciones para que el juego funcione correctamente

--- Adivinar_numero.py
+++ Adivinar_numero.py
@@ -4,7 +4,17 @@ num = random.randint(0,99)   # Numero aleatorio a adivinar
 contador = 0   # En esta variable se almacenara el numero de intentos
 
 while num:
-    intento = int (input ("Introduzca un numero, entre 0 y 99, ambos incluidos: "))
-    if 0 > intento > 99:
+    intento = int (input ("Introduzca un numero, entre 0 y 99, ambos incluidos: "))   # Le pedimos al usuario que introduzca un numero para adivinar el numero aleatorio
+    if 0 > intento > 99:   # Nos aseguramos d que el numero esta comprendido dentro del rango que queremos
         print ("Introduzca un valor entre 0 y 99 por favor: ")
-    
+    elif intento < num:
+        print ("Demasiado pequeño: ")
+        contador += 1
+    elif intento > num:
+        print ("Demasiado grande")
+        contador += 1
+    else:
+        print ("¡Ha ganado! ✌")
+        contador += 1
+        num = False   # Cerramos el bucle while
+        print ("Ha necesitado ", contador, " intentos para conseguirlo")
