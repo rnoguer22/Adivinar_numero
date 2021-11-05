c3c6a7cfb7b8a28d9ea6984d6a6fba109d537ae8
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-05 09:46:34 +0100

    Creamos el bucle while Le pedimos al usuario que introduzca un numero para comenzar el juego

--- Adivinar_numero.py
+++ Adivinar_numero.py
@@ -2,3 +2,9 @@ import random
 num = random.randint(0,99)   # Numero aleatorio a adivinar
 
 contador = 0   # En esta variable se almacenara el numero de intentos
+
+while num:
+    intento = int (input ("Introduzca un numero, entre 0 y 99, ambos incluidos: "))
+    if 0 > intento > 99:
+        print ("Introduzca un valor entre 0 y 99 por favor: ")
+    
